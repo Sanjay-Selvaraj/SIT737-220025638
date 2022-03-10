0588f6a6690e658fdf041f9f9d8d067a14735c03
Sanjay Selvaraj <sanjayselvaraj98@gmail.com>
2022-03-10 18:34:22 +1100

    init

---
+++ func1.js
@@ -0,0 +1,5 @@
+var log=function(msg){
+    console.log("[Log] : ",msg)
+   }
+   log("hello world")
+   log("welcome to SIT737")
---
+++ func2.js
@@ -0,0 +1,10 @@
+var adder=function(first,second){
+    var sum=first+second
+    return sum
+   }
+   var log=function(msg){
+    console.log("[Log] : ",msg)
+   }
+   log("hello world")
+   log("welcome to SIT737")
+   log("The sum is "+adder(5,6))
