448b15b0c2ca7dd3c274aa95c394d1bda5b0cc85
Sanjay Selvaraj <sanjayselvaraj98@gmail.com>
2022-03-11 18:45:01 +1100

    log function

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 var express = require('express')
+
+const log4js = require("log4js");
+log4js.configure({
+  appenders: { logs: { type: "file", filename: "logs.log" } },
+  categories: { default: { appenders: ["logs"], level: "info" } }
+});
+
+const logger = log4js.getLogger("logs");
+
+
+
 var app = express()
+logger.info("Server Started");
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
  res.send('hello world')
+
+
+ 
+
+ logger.info("Server response sent");
+
 })
+
+
 // list to a particular port
 app.listen(3000)
+
